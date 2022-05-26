ea9b4ecc01ffa8123b220578705ea864b18431f3
54rtor1 <1929sartori@gmail.com>
2022-05-26 18:22:43 -0300

    movie validation update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :lists, through: :bookmarks
 
+  validates :title, presence: true, uniqueness: true
   validates :title, :overview, presence: true
 end
