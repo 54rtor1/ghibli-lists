bedc49aef7e35ca8752ca22ef0bc3d74e2689eb9
54rtor1 <1929sartori@gmail.com>
2022-05-26 15:55:39 -0300

    seeds ghibli movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database" if Rails.env.development?
+
+Movie.destroy_all
+
+puts 'Database Clean'
+
+puts 'Creating Movies'
+
+6.times do
+  movie = Movie.create(
+    title: Faker::JapaneseMedia::StudioGhibli.movie,
+    overview: Faker::JapaneseMedia::StudioGhibli.quote,
+    rating: (1..10).to_a.sample,
+    poster_url: ['https://lwlies.com/wp-content/uploads/2016/06/1984-Kaze-no-tani-no-Naushika-Nausicaa-del-Valle-del-Viento-Guerreros-del-viento-jap-03-900x0-c-default.jpg', 'https://lwlies.com/wp-content/uploads/2016/06/my_neighbor_totoro_poster-900x0-c-default.jpg', 'https://lwlies.com/wp-content/uploads/2016/06/princess_mononoke_poster-900x0-c-default.jpg', 'https://lwlies.com/wp-content/uploads/2016/06/TheWindRisesPoster-900x0-c-default.jpg',' https://lwlies.com/wp-content/uploads/2016/06/ee80632a9278d3e1f6aae9f8cc9b509a-900x0-c-default.jpg', 'https://lwlies.com/wp-content/uploads/2016/06/howls-moving-castle-poster-900x0-c-default.png'].sample
+  )
+  puts movie.title
+end
+
+puts "Finished"
