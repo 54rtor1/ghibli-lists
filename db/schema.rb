6acda6e10b724c352a5b9e2bc90ba1666f11496c
54rtor1 <1929sartori@gmail.com>
2022-05-26 13:43:28 -0300

    ratings added to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_163034) do
+ActiveRecord::Schema.define(version: 2022_05_26_164227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_05_26_163034) do
     t.string "poster_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
   end
 
   add_foreign_key "bookmarks", "lists"
